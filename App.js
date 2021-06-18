4059bc0c7328faed98c4c77d15d314c0641504da
msun4100 <msun4100@gmail.com>
2021-06-18 16:03:15 +0900

    feat: vector-icons setup on android/ios

--- App.js
+++ App.js
@@ -8,6 +8,21 @@
 
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <Text>Project Setup</Text>
+      <Icon name="cake" size={170} />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   sectionContainer: {
